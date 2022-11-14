945c970cd633daf1b1a624603f71bca8ac1ce57e
Gesztesi Szabolcs <gesztesi11@gmail.com>
2022-11-14 19:38:16 +0100

    middle uasc tested

--- main.ino
+++ main.ino
@@ -2,16 +2,16 @@
 #define trig_left 2
 #define echo_left 3
 
-long duration_left;
-int distance_left;
-
 void setup(){
   pinMode(trig_left, OUTPUT);
   pinMode(echo_left, INPUT);
+  pinMode(trig_middle, OUTPUT);
+  pinMode(echo_middle, INPUT);
   Serial.begin(9600);
 }
 
 
 void loop (){
   uasc_measure_left();
+  uasc_measure_middle(){;
 }
--- uasc.h
+++ uasc.h
@@ -1,8 +1,12 @@
 #define trig_left 2
 #define echo_left 3
+#define trig_middle 4
+#define echo_middle 5
 
 long duration_left;
 int distance_left;
+long duration_middle;
+int distance_middle;
 
 void uasc_measure_left(){
     digitalWrite(trig_left, LOW);
@@ -14,5 +18,17 @@ void uasc_measure_left(){
     distance_left = duration_left * 0.034 / 2;
     Serial.print("Distance: ");
     Serial.print(distance_left);
+    Serial.println(" cm");
+}
+void uasc_measure_middle(){
+    digitalWrite(trig_middle, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig_middle, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig_middle, LOW);
+    duration_middle = pulseIn(echo_middle, HIGH);
+    distance_middle = duration_middle * 0.034 / 2;
+    Serial.print("Distance: ");
+    Serial.print(distance_middle);
     Serial.println(" cm");
 }
