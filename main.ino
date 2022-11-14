ab4e8cefe86950081c0b587ee6628c9d7c941641
Gesztesi Szabolcs <gesztesi11@gmail.com>
2022-11-14 19:52:21 +0100

    right uasc must be test more

--- main.ino
+++ main.ino
@@ -1,17 +1,21 @@
 #include "uasc.h"
-#define trig_left 2
-#define echo_left 3
+
 
 void setup(){
   pinMode(trig_left, OUTPUT);
   pinMode(echo_left, INPUT);
   pinMode(trig_middle, OUTPUT);
   pinMode(echo_middle, INPUT);
+  pinMode(trig_right, OUTPUT);
+  pinMode(echo_right, INPUT);
   Serial.begin(9600);
 }
 
 
 void loop (){
   uasc_measure_left();
-  uasc_measure_middle(){;
+  uasc_measure_middle();
+  uasc_measure_right();
+  Serial.println();
+  delay(50);
 }
--- uasc.h
+++ uasc.h
@@ -2,33 +2,49 @@
 #define echo_left 3
 #define trig_middle 4
 #define echo_middle 5
+#define trig_right 6
+#define echo_right 7
 
-long duration_left;
-int distance_left;
-long duration_middle;
-int distance_middle;
+volatile long duration_left;
+volatile int distance_left;
+volatile long duration_middle;
+volatile int distance_middle;
+volatile long duration_right;
+volatile int distance_right;
 
-void uasc_measure_left(){
+inline void uasc_measure_left(){
     digitalWrite(trig_left, LOW);
     delayMicroseconds(2);
     digitalWrite(trig_left, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig_left, LOW);
     duration_left = pulseIn(echo_left, HIGH);
-    distance_left = duration_left * 0.034 / 2;
+    distance_left = duration_left * 0.017;
     Serial.print("Distance: ");
     Serial.print(distance_left);
     Serial.println(" cm");
 }
-void uasc_measure_middle(){
+inline void uasc_measure_middle(){
     digitalWrite(trig_middle, LOW);
     delayMicroseconds(2);
     digitalWrite(trig_middle, HIGH);
     delayMicroseconds(10);
     digitalWrite(trig_middle, LOW);
     duration_middle = pulseIn(echo_middle, HIGH);
-    distance_middle = duration_middle * 0.034 / 2;
+    distance_middle = duration_middle * 0.017;
     Serial.print("Distance: ");
     Serial.print(distance_middle);
+    Serial.println(" cm");
+}
+inline void uasc_measure_right(){
+    digitalWrite(trig_right, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig_right, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig_right, LOW);
+    duration_right = pulseIn(echo_right, HIGH);
+    distance_right = duration_right * 0.017;
+    Serial.print("Distance: ");
+    Serial.print(distance_right);
     Serial.println(" cm");
 }
