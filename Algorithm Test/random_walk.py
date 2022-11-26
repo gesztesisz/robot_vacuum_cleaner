35b221e88384c9f69a5a3b664ac2d5f5558908e6
Gesztesi Szabolcs <gesztesi11@gmail.com>
2022-11-26 10:45:12 +0100

    1.0 DONE

--- Algorithm Test/random_walk.py
+++ Algorithm Test/random_walk.py
@@ -3,6 +3,7 @@ import random
 import time
 
 robot_size = 10
+step = 0.5
 map_x = 500
 map_y = 500 
 pygame.init()
@@ -14,21 +15,45 @@ robot_color=(255,0,0)
 
 x = map_x/2 # start x
 y = map_y/2 # start y
+robot_size = 10
+step = 0.5
+
 
 directions = ['up','down','left','right']
-
+direc = random.choice(directions)
 
 while True:
-    direc = random.choice(directions)
+    #direc = random.choice(directions)
     if 	 direc == 'up'   :
-        y += -0.1
+        #next_y += -0.1
+        if (y  - step - robot_size) <= 0:
+            direc = random.choice(directions)
+            continue
+        else:
+            y -= step
     elif direc == 'down' :
-        y += 0.1
+        #next_y += 0.1
+        if (y  + step + robot_size) >= map_y:
+            direc = random.choice(directions)
+            continue
+        else:
+            y += step
     elif direc == 'left' :
-        x += -0.1
+        #next_x += -0.1
+        if (x  - step - robot_size) <= 0:
+            direc = random.choice(directions)
+            continue
+        else:
+            x -= step
     elif direc == 'right':
-        x += 0.1
-
+        #next_x += 0.1
+        if (x  + step + robot_size) >= map_x:
+            direc = random.choice(directions)
+            continue
+        else:
+            x += step
+    
+    
     dis.fill(background)
     pygame.draw.circle(dis, robot_color, (x,y), robot_size) 
     pygame.display.update()
