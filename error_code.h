0368175fdf76da5bc9fa376b142608103913f85d
gesztesisz <55894983+gesztesisz@users.noreply.github.com>
2022-11-29 09:28:14 +0100

    Update error_code.h

--- error_code.h
+++ error_code.h
@@ -1,7 +1,7 @@
 int priority_array[4] = {1,2,3,4}; //1 -> HIGH Priority, 4 -> LOW priority
 int priority = 0;
 
-void error_code (int code){
+int error_code (int code){
   switch(code){
     case 1: 
       priority =  priority_array[1]; // lvl 2 priority
@@ -10,4 +10,5 @@ void error_code (int code){
     break;
     
   }
+  return priority;
 }
