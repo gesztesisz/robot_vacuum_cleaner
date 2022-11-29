691862f26c0a2de3cdfe3b3f7d32b6dbcb0d8807
gesztesisz <55894983+gesztesisz@users.noreply.github.com>
2022-11-29 09:24:18 +0100

    error_code added

---
+++ error_code.h
@@ -0,0 +1,12 @@
+int priority[4] = {1,2,3,4}; //1 -> HIGH Priority, 4 -> LOW priority
+
+void error_code (int code){
+  switch(code){
+    case 0: 
+    
+    break;
+    default:
+    break;
+    
+  }
+}
