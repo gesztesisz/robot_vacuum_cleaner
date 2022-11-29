12ebb74e6630d9353431b60a6bc0c9d9be80761a
gesztesisz <55894983+gesztesisz@users.noreply.github.com>
2022-11-29 09:26:35 +0100

    delay error handled

--- error_code.h
+++ error_code.h
@@ -1,9 +1,10 @@
-int priority[4] = {1,2,3,4}; //1 -> HIGH Priority, 4 -> LOW priority
+int priority_array[4] = {1,2,3,4}; //1 -> HIGH Priority, 4 -> LOW priority
+int priority = 0;
 
 void error_code (int code){
   switch(code){
-    case 0: 
-    
+    case 1: 
+      priority =  priority_array[1]; // lvl 2 priority
     break;
     default:
     break;
